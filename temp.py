505f7c480ab6aeff01e9dd7d2af0bf23c1e8190e
Patrick <patrickjroome@gmail.com>
2020-11-15 01:24:48 +0000

    yuh

---
+++ temp.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+banana = np.pi + 231
+apple = 17
+
+x = np.linspace(0,1,10)
+y = x = np.linspace(0,3,10)
+
+plt.errorbar(x,y, fmt='o')
+
+print(banana, apple)
