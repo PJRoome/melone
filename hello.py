55f1d01b6ea5b7b1801d90a8ac953e338f01523a
Patrick <patrickjroome@gmail.com>
2020-11-15 01:26:03 +0000

    delete

--- hello.py
+++
@@ -1,5 +0,0 @@
-Python 3.6.6 (v3.6.6:4cf1f54eb7, Jun 27 2018, 02:47:15) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> print('hello')
-hello
->>> 
