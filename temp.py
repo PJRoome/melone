764846faacda68c06079873bbbb214611e9d2378
Patrick <patrickjroome@gmail.com>
2020-11-15 01:23:26 +0000

    graph

--- temp.py
+++ temp.py
@@ -1,5 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 banana = np.pi + 231
+apple = 17
 
-print(banana)
+x = np.linspace(0,1,100)
+y = x = np.linspace(0,3,100)
+
+plt.errorbar(x,y)
+plt.show()
+print(banana, apple)
