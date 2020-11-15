53a4725231ab9f34fb6e70cd77980d387830fd92
Patrick <patrickjroome@gmail.com>
2020-11-15 01:24:48 +0000

    yuh

--- temp.py
+++ temp.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 banana = np.pi + 231
 apple = 17
 
-x = np.linspace(0,1,100)
-y = x = np.linspace(0,3,100)
+x = np.linspace(0,1,10)
+y = x = np.linspace(0,3,10)
 
-plt.errorbar(x,y)
-plt.show()
+plt.errorbar(x,y, fmt='o')
+
 print(banana, apple)
